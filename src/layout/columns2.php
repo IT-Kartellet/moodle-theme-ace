03f27422fbacd2d5b3f8430387d6160dc0a1f9c9
Andrei Neculaesei <andrew@it-kartellet.dk>
2014-03-27 16:55:36 +0100

    2 column template for desktop and mobile

--- src/layout/columns2.php
+++ src/layout/columns2.php
@@ -4,22 +4,34 @@
     <title><?php echo $OUTPUT->page_title();?></title>
     <link rel="shortcut icon" href="<?=$OUTPUT->favicon()?>" />
     <?php echo $OUTPUT->standard_head_html();?>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 </head>
 <body <?php echo $OUTPUT->body_attributes();?>>
 <?php echo $OUTPUT->standard_top_of_body_html();?>
 
-<div id="main_wrap">
+<div id="primary_wrap" class="columns-2">
+<div id="secondary_wrap">
 
 <header id="main_header" role="banner" class="container-fluid">
-    <div class="row-fluid">
-        <a id="logo_wrap" class="span2" href="<?php echo $CFG->wwwroot; ?>">
+    <div id="mobile_header" class="visible-mobile">
+        <div id="mobile_header_left">
+            <span class="pull-left">Menu</span>
+            <button class="pull-right navigate_content"><i class="icon-right-open"></i></button>
+        </div>
+        <div id="mobile_header_middle">
+            <button class="pull-left navigate_menu"><i class="icon-menu-1"></i></button>
+            <span id="mobile_middle_title">Home</span>
+        </div>
+    </div>
+
+    <div class="row-fluid hidden-mobile">
+        <a id="logo_wrap" class="span2 hidden-mobile" href="<?php echo $CFG->wwwroot; ?>">
             <img id="logo" src='<?=$CFG->wwwroot.'/theme/'.$PAGE->theme->name."/pix/logo.svg"?>'>
         </a>
     </div>
 </header>
 
-<div id="page" class="container-fluid columns-2">
+<div id="page" class="container-fluid">
     <div id="page-content" class="row-fluid">
         <div id="page_wrapper" class="span12">
             <?php echo $OUTPUT->blocks('side-pre', '');?>
